3de6dfe3248a76b51438f08dfa2e6289d9811f13
DESKTOP-43IUE12\Student <mendy@famkahan.com>
2018-07-18 13:38:31 +0100

    test

--- includes/config.php
+++ includes/config.php
@@ -2,6 +2,6 @@
 <?php
 	defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
 	defined('DB_USER')   ? null : define("DB_USER", "root");
-	defined('DB_PASS')   ? null : define("DB_PASS", "p9cgkp9cgk");
+	defined('DB_PASS')   ? null : define("DB_PASS", "P9cgkp9cgk");
 	defined('DB_NAME')   ? null : define("DB_NAME", "property_management");
 ?>
